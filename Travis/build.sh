38d9ef97834fc407d4d854885aee2616ac6e6507
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2019-05-16 15:42:38 +0200

    Initial travis script

---
+++ Travis/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(dirname $0)
+
+OS_BUILD_SCRIPT="${SCRIPT_DIR}/build-${TRAVIS_OS_NAME}.sh"
+
+file $OS_BUILD_SCRIPT
+if [ ! -f ${OS_BUILD_SCRIPT} ]; then
+    echo "OS build script does not exist: '${OS_BUILD_SCRIPT}'"
+    exit 1
+fi
+
+echo "Running script '${OS_BUILD_SCRIPT}' for '${TRAVIS_OS_NAME}'"
+${OS_BUILD_SCRIPT}
