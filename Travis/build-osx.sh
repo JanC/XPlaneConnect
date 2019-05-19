67b185901e5e665755ec054ecd5cfdf6543484b9
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2019-05-19 18:12:58 +0200

    s3 testing zip

--- Travis/build-osx.sh
+++ Travis/build-osx.sh
@@ -5,9 +5,11 @@ SCRIPT_DIR=$(dirname $0)
 
 echo "Build macOS"
 cd $XPC_PLUGIN_PATH
-xcodebuild clean build CONFIGURATION_BUILD_DIR="."
+# xcodebuild clean build CONFIGURATION_BUILD_DIR="."
 
 BINRAY=${PWD}/mac.xpl
+
+cp ../xpcPlugin/XPlaneConnect/mac.xpl ~/$TRAVIS_BUILD_NUMBER
 
 if [ -f ${BINRAY} ]; then
     echo "## Copying macOS Binray ##"
