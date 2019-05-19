9a53e24546f304be4afcd2d8268b5b2b34cb99f8
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2019-05-19 19:24:36 +0200

    copy binaries

---
+++ Travis/copy-binaries.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname $0)
+. ${SCRIPT_DIR}/.env
+
+echo "Build macOS"
+cd $XPC_PLUGIN_PATH
+
+if [ "$TRAVIS_OS_NAME" == "osx"   ] ; then
+    BINRAY=${PWD}/mac.xpl
+    cp ../xpcPlugin/XPlaneConnect/mac.xpl ~/$TRAVIS_BUILD_NUMBER
+    if [ -f ${BINRAY} ]; then
+        echo "## Copying macOS Binray ##"
+        echo "${BINRAY}"
+        cp "${BINRAY}" ~/$TRAVIS_BUILD_NUMBER
+    fi
+fi
+
+if [ "$TRAVIS_OS_NAME" == "osx"   ] ; then
+    # pwd
+    # ls -al
+    cp ../xpcPlugin/XPlaneConnect/lin.xpl ~/$TRAVIS_BUILD_NUMBER
+    cp ../xpcPlugin/XPlaneConnect/64/lin.xpl ~/$TRAVIS_BUILD_NUMBER/64/
+
+    if [ -f "${PWD}/xpc64.xpl" ]; then
+        echo "##  Copying Linux Binaries ##"
+        cp "${PWD}/xpc32.xpl" ~/$TRAVIS_BUILD_NUMBER/
+        cp "${PWD}/xpc64.xpl" ~/$TRAVIS_BUILD_NUMBER/64/
+    fi
+fi
+
+if [ "$TRAVIS_OS_NAME" == "windows"   ] ; then
+fi
+
