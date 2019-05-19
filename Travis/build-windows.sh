768206120e5d2c19a71c908441abd1a25dd7181f
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2019-05-19 19:51:56 +0200

    win tests

--- Travis/build-windows.sh
+++ Travis/build-windows.sh
@@ -3,7 +3,16 @@
 # MSBuild.exe xpcPlugin/xpcPlugin/xpcPlugin.sln /p:Configuration=Release
 # MSBuild.exe xpcPlugin/xpcPlugin/xpcPlugin.sln /p:Configuration=Release /p:Platform=x64
 
-pwd
-MSBuild.exe
-ls
-choco install awscli
+echo "Building Windows"
+MSBuild.exe xpcPlugin/xpcPlugin/xpcPlugin.sln /p:Configuration=Release
+echo "Done"
+
+# echo $PATH
+# export PATH=$MSBUILD_PATH:$PATH
+# echo $PATH
+# # pwd
+# # MSBuild.exe
+
+# ls -al "$MSBUILD_PATH"
+# MSBuild.exe --help
+# choco install awscli
