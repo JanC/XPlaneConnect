f5449f01bb67d9c056f10b41dd1eb5796a4848e7
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2019-05-19 18:56:19 +0200

    s3 working full

--- Travis/build-linux.sh
+++ Travis/build-linux.sh
@@ -11,22 +11,18 @@ if [ ! -f /usr/include/asm ]; then
     ln -s /usr/include/asm-generic/ /usr/include/asm
 fi
 
-# pass the compiler version from the Travis env
-# cmake -D CMAKE_C_COMPILER=${CC} -D CMAKE_CXX_COMPILER=${CXX} .
-# make
+pass the compiler version from the Travis env
+cmake -D CMAKE_C_COMPILER=${CC} -D CMAKE_CXX_COMPILER=${CXX} .
+make
 
 
-pwd
-ls -al
-cp ../xpcPlugin/XPlaneConnect/lin.xpl ~/$TRAVIS_BUILD_NUMBER
-cp ../xpcPlugin/XPlaneConnect/64/lin.xpl ~/$TRAVIS_BUILD_NUMBER/64/
+# pwd
+# ls -al
+# cp ../xpcPlugin/XPlaneConnect/lin.xpl ~/$TRAVIS_BUILD_NUMBER
+# cp ../xpcPlugin/XPlaneConnect/64/lin.xpl ~/$TRAVIS_BUILD_NUMBER/64/
 
 if [ -f "${PWD}/xpc64.xpl" ]; then
     echo "##  Copying Linux Binaries ##"
-    # echo "${PWD}/xpc32.xpl"
-    # echo "${PWD}/xpc64.xpl"
-    # cp "${PWD}/xpc32.xpl" ~/$TRAVIS_BUILD_NUMBER
-    # cp "${PWD}/xpc64.xpl" ~/$TRAVIS_BUILD_NUMBER
-    
-    cp ${$XPC_PLUGIN_PATH}/XPlaneConnect/lin.xpl ~/$TRAVIS_BUILD_NUMBER
+    cp "${PWD}/xpc32.xpl" ~/$TRAVIS_BUILD_NUMBER/
+    cp "${PWD}/xpc64.xpl" ~/$TRAVIS_BUILD_NUMBER/64/
 fi
