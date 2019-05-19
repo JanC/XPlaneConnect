c3e98f896a50e3a230bdbe4466931df24e9eb776
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2019-05-19 20:54:44 +0200

    GH releases

--- Travis/build-osx.sh
+++ Travis/build-osx.sh
@@ -5,16 +5,6 @@ SCRIPT_DIR=$(dirname $0)
 
 echo "Build macOS"
 cd $XPC_PLUGIN_PATH
-# xcodebuild clean build CONFIGURATION_BUILD_DIR="."
-
-BINRAY=${PWD}/mac.xpl
-
-cp ../xpcPlugin/XPlaneConnect/mac.xpl ~/$TRAVIS_BUILD_NUMBER
-
-if [ -f ${BINRAY} ]; then
-    echo "## Copying macOS Binray ##"
-    echo "${BINRAY}"
-    cp "${BINRAY}" ~/$TRAVIS_BUILD_NUMBER
-fi
+xcodebuild clean build CONFIGURATION_BUILD_DIR="."
 
 
