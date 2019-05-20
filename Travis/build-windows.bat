4d4fe00d4120df93afcbdb5e78776d3a7e8c26b6
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2019-05-20 08:27:52 +0200

    win tests

--- Travis/build-windows.bat
+++ Travis/build-windows.bat
@@ -1,3 +1,3 @@
 
-cmd.exe /C 'MSBuild.exe xpcPlugin/xpcPlugin/xpcPlugin.sln /p:Configuration=Release'
+cmd.exe /C '/C/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin/MSBuild.exe xpcPlugin/xpcPlugin/xpcPlugin.sln /p:Configuration=Release'
 REM MSBuild.exe xpcPlugin/xpcPlugin/xpcPlugin.sln /p:Configuration=Release /p:Platform=x64
