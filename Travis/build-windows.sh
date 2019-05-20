6a9103ce33f853f93db763293a7dd5b5d3ef718c
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2019-05-20 08:45:38 +0200

    win tests

--- Travis/build-windows.sh
+++ Travis/build-windows.sh
@@ -13,7 +13,10 @@ ls -al /usr/bin
 #file $MSBUILD_PATH/MSBuild.exe
 echo "PATH: $PATH"
 
-MSBuild.exe xpcPlugin/xpcPlugin/xpcPlugin.sln //p:Configuration=Release
+echo "ls studio"
+ls -al "/C/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin"
+
+cmd.exe /C '/C/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/MSBuild/15.0/Bin/MSBuild.exe "xpcPlugin/xpcPlugin/xpcPlugin.sln /p:Configuration=Release'
 echo "Done"
 
 # echo $PATH
